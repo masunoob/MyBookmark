eb9250a71733d9395989c9bd033fe01c24511ffe
masunoob <tomoshima0313@gmail.com>
2025-07-18 22:12:40 +0900

    Bookmarkモデルの作成と一覧ページの作成を行いました

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Bookmark
 
 # Create your views here.
-def index(request):
-    return render(request, 'main_app/index.html')
+def bookmark_list(request):
+    bookmarks = Bookmark.objects.all()
+    context = {
+        'bookmarks': bookmarks
+    }
+    return render(request, 'main_app/bookmark_list.html', {'context': context})
