f407bf858421f603c7e2fac8772046311d2f64ce
masunoob <tomoshima0313@gmail.com>
2025-07-18 22:25:18 +0900

    ブックマーク作成ページの実装を行いました。

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.bookmark_list, name='bookmark_list'),
+    path('create/', views.bookmark_create, name='bookmark_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Bookmark
+from .forms import BookmarkForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def bookmark_list(request):
@@ -8,3 +10,13 @@ def bookmark_list(request):
         'bookmarks': bookmarks
     }
     return render(request, 'main_app/bookmark_list.html', {'context': context})
+
+def bookmark_create(request):
+    if request.method == 'POST':
+        form = BookmarkForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('bookmark_list')
+    else:
+        form = BookmarkForm()
+    return render(request, 'main_app/bookmark_form.html', {'form': form})
