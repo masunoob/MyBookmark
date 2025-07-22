dabb66488c11cdb8c96d2f6ec4eba955019e8de6
masunoob <tomoshima0313@gmail.com>
2025-07-22 22:05:50 +0900

    ブックマーク一覧ページでクリック時の挙動をjsで実装しました

---
+++ main_app/static/main_app/js/pages/bookmark_list.js
@@ -0,0 +1,58 @@
+class BookmarkList {
+  constructor() {
+    this.init();
+    this.bookmarkList;
+    this.editButton;
+    this.deleteButton;
+  }
+
+  init() {
+    // ブックマークリスト
+    this.bookmarkList = document.querySelectorAll('.bookmark-card-content');
+    this.bookmarkList.forEach(bookmark => {
+      bookmark.addEventListener('click', this.handleArticleClick.bind(this));
+    });
+
+    // 編集ボタン
+    this.editButton = document.querySelectorAll('.editButton');
+    this.editButton.forEach(button => {
+      button.addEventListener('click', this.handleEditButtonClick.bind(this));
+    });
+
+    // 削除ボタン
+    this.deleteButton = document.querySelectorAll('.deleteButton');
+    this.deleteButton.forEach(button => {
+      button.addEventListener('click', this.handleDeleteButtonClick.bind(this));
+    });
+  }
+
+  // ブックマークカードのクリックイベント
+  handleArticleClick(event) {
+    const url = event.currentTarget.getAttribute('data-url');
+    window.open(url, '_blank');
+  }
+
+  // 編集ボタンのクリックイベント
+  handleEditButtonClick(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const url = event.currentTarget.getAttribute('data-url');
+    const id = event.currentTarget.getAttribute('data-id');
+    window.location.href = `${url}?id=${id}`;
+  }
+
+  // 削除ボタンのクリックイベント
+  handleDeleteButtonClick(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const url = event.currentTarget.getAttribute('data-url');
+    const id = event.currentTarget.getAttribute('data-id');
+    window.location.href = `${url}?id=${id}`;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new BookmarkList();
+});
