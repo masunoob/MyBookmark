0a6a1dd055f61c94316cb0c91c435a12907aacc2
masunoob <tomoshima0313@gmail.com>
2025-07-24 22:28:50 +0900

    管理画面でタグも確認できるように対応

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Bookmark
+from .models import Bookmark, Tag
 
 # Register your models here.
 admin.site.register(Bookmark)
-
+admin.site.register(Tag)
